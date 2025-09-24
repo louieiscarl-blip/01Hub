36271a8f1860ae2c9a5f78456ae81416886d9e16
louieiscarl-blip <louieiscarl@gmail.com>
2025-09-24 14:07:11 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,67 +1,20 @@
---// Loading Libary
-local success, lib = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/louieiscarl-blip/01Hub/main/Libary.lua"))()
-end)
+-- 01Hub v1.8 (WindUI Edition)
 
-if not success or not lib then
-    warn("[01Hub] Failed to load Libary.lua")
-    return
-end
-
---// Creating Menu
-local menu = lib:CreateMenu({
+local Window = WindUI:CreateWindow({
     Title = "01Hub v1.8",
-    Style = 1
+    Theme = "Dark",
+    Folder = "01Hub"
 })
 
---// Tab: Movements
-local MovementsTab = menu:CreateTab("Movements")
-
-local MoveSection = MovementsTab:AddSection("Player Movement", 1)
-MoveSection:AddSlider({
-    text = "WalkSpeed",
-    min = 1, max = 200, value = 16, increment = 1,
-    callback = function(val)
-        local char = game.Players.LocalPlayer.Character
-        if char then
-            local hum = char:FindFirstChildOfClass("Humanoid")
-            if hum then
-                hum.WalkSpeed = val
-            end
-        end
+-- Movements Tab
+local MovTab = Window:Tab({ Title = "Movements", Icon = "zap" })
+MovTab:Slider({
+    Title = "WalkSpeed",
+    Min = 16,
+    Max = 200,
+    Default = 16,
+    Callback = function(val)
+        local hum = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+        if hum then hum.WalkSpeed = val end
     end
 })
-
-MoveSection:AddSlider({
-    text = "JumpPower",
-    min = 20, max = 400, value = 50, increment = 1,
-    callback = function(val)
-        local char = game.Players.LocalPlayer.Character
-        if char then
-            local hum = char:FindFirstChildOfClass("Humanoid")
-            if hum then
-                hum.UseJumpPower = true
-                hum.JumpPower = val
-            end
-        end
-    end
-})
-
---// Tab: Visuals
-local VisualsTab = menu:CreateTab("Visuals")
-local VisualsSec = VisualsTab:AddSection("ESP", 1)
-
-VisualsSec:AddToggle({
-    text = "Player ESP",
-    callback = function(state)
-        if state then
-            print("ESP enabled")
-            -- add ESP logic here
-        else
-            print("ESP disabled")
-        end
-    end
-})
-
---// Notification
-lib:SendNotification("01Hub", "Loaded v1.8 suc
