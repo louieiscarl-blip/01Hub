eb64ee43807b5cba03ac37d6c35f495810d2d1ee
louieiscarl-blip <louieiscarl@gmail.com>
2025-09-24 13:47:58 +0800

    Update 01Hub.lua

--- 01Hub.lua
+++ 01Hub.lua
@@ -1,2 +1,5 @@
--- 01Hub.lua fancy
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/louieiscarl-blip/01Hub/main/Source.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/louieiscarl-blip/01Hub/main/Library.lua"))()
+local menu = lib:CreateWindow("01Hub v1.8")
+
+local MainTab = menu:CreateTab("Main")
+MainTab:CreateLabel("Loaded successfully!")
